1a20b55a3652e6519ce9221709775861e3c5f9da
Matthew King <matthew.king@monnsta.net>
2014-02-27 14:20:16 +0000

    Don't attempt to test empty directories.

--- check_puppet_spec_tests/run_spec_tests.sh
+++ check_puppet_spec_tests/run_spec_tests.sh
@@ -17,18 +17,18 @@ The second argument should be a folder containing puppet modules.
 Spec folders are searched recursively. Value by default is "/etc/puppet"
 
 EOHELP
-  exit 0;
+  exit 0
 }
 
 syserr() {
   echo "$0: ERROR: $*" 1>&2
-  exit 1;
+  exit 1
 }
 
 ## No arguments
-if [ ${#*} == 0 ]; then
+if [[ ${#*} -eq 0 ]]; then
   _help
-fi;
+fi
 
 case "$1" in
 	full)            ACTION="rake spec";;
@@ -38,29 +38,47 @@ case "$1" in
 	*)               syserr "Command option '$1' not recognized";;
 esac
 
-PUPPET_ROOT="/etc/puppet";
-[ -z $2 ] || PUPPET_ROOT=$2
-MODULE_FOLDERS_WITH_SPECS=$(find ${PUPPET_ROOT} -type d -name spec -exec sh -c 'dirname {}' \;);
-MODULES_FAILING=""
-[ -z $2 ] || PUPPET_ROOT=$2
+# Set PUPPET_ROOT to $2 or /etc/puppet
+#
+PUPPET_ROOT=${2:-/etc/puppet}
 
-if [ -z "${MODULE_FOLDERS_WITH_SPECS}" ]; then
+# Include each folder which has a spec folder which is not empty.
+#
+unset MODULE_FOLDERS_WITH_SPECS
+while read path; do
+  if [[ $(/bin/ls -1 "$path" | wc -l) -ne 0 ]]; then
+    MODULE_FOLDERS_WITH_SPECS+=("$(dirname "$path")")
+  fi
+done < <(find ${PUPPET_ROOT} -type d -name spec -print)
+
+# Leave early if there's nothing to do.
+#
+if [[ ${#MODULE_FOLDERS_WITH_SPECS} -eq 0 ]]; then
   echo "No specs were found."
-  exit 0;
-fi; 
+  exit 0
+fi
 
-for module in ${MODULE_FOLDERS_WITH_SPECS}; do
+# Run each test, collecting failures.
+#
+unset MODULES_FAILING
+for module in "${MODULE_FOLDERS_WITH_SPECS[@]}"; do
+  module_name=${module#$PUPPET_ROOT}
 
-  error=0
-  cd ${module}
-  echo "The current module is ${module}..."
-  ${ACTION} || error=1
-
-  [ "$error" == "0" ] || MODULES_FAILING="${MODULES_FAILING}\n${module}"
+  pushd "${module}" >/dev/null
+  echo "The current module is ${module_name}..."
+  if ! ${ACTION}; then
+    MODULES_FAILING+=("${module_name}")
+  fi
+  popd >/dev/null
 
 done
 
-if [ ! -z "${MODULES_FAILING}" ]; then
-  echo -e "There were errors in the next modules: ${MODULES_FAILING}";
-  exit 1;
+# Complain noisily if there were problems.
+#
+if [[ ${#MODULES_FAILING} -ne 0 ]]; then
+  echo "The following modules had errors:"
+  for module in "${MODULES_FAILING[@]}"; do
+    echo "  $module"
+  done
+  exit 1
 fi
